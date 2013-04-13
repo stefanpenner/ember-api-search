1a86f8cbc648c4d3699d806d8a3967fd142ec5b2
toran billups <toranb@gmail.com>
2013-04-13 14:30:27 -0500

    wip api search

--- tests/api_search_tests.js
+++ tests/api_search_tests.js
@@ -1,4 +1,42 @@
-test("hello", function(){
-  expect(1);
-  ok(search, 'failure');
+module("ApiSearch public api");
+
+function buildApiSearch (data) {
+  return new ApiSearch(data);
+}
+
+test("namespace", function(){
+  ok(ApiSearch);
+});
+
+test("instantiation", function(){
+  var instance;
+
+  instance = buildApiSearch();
+
+  ok(instance);
+  ok(instance.search);
+});
+
+module("ApiSearch.search");
+
+test("empty query", function(){
+  var instance;
+
+  instance = buildApiSearch();
+
+  deepEqual(instance.search(), {}, 'no results');
+});
+
+test("simple search, with no data", function(){
+  var result, query, apiSearch;
+
+  instance = buildApiSearch();
+
+  query = "Ember.View";
+  result = instance.search(query);
+
+  deepEqual(result.files,      [], 'empty files');
+  deepEqual(result.classes,    [], 'empty classes');
+  deepEqual(result.modules,    [], 'empty modules');
+  deepEqual(result.classItems, [], 'empty classitems');
 });
